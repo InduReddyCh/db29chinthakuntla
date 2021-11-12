5f4fc12326ad9783ac27d213a04897d14c3da518
InduReddyCh <S545151@nwmissouri.edu>
2021-11-12 11:45:23 -0600

    modified controller

--- controllers/ball.js
+++ controllers/ball.js
@@ -4,8 +4,15 @@ exports.ball_list = function (req, res) {
     res.send('NOT IMPLEMENTED: ball list');
 };
 // for a specific ball.
-exports.ball_detail = function (req, res) {
-    res.send('NOT IMPLEMENTED: ball detail: ' + req.params.id);
+exports.ball_detail =async function (req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await ball.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
 };
 // Handle ball create on POST.
 exports.ball_create_post = async function (req, res) {
--- routes/ball.js
+++ routes/ball.js
@@ -1,9 +1,10 @@
 var express = require('express');
+const ball_controller=require('../controllers/ball');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('ball', { title: 'Search Results Ball' });
 });
-
+router.get('/ball/:id', ball_controller.ball_detail);
 module.exports = router;
