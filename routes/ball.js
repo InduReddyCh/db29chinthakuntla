fb3700c068b9343c0d83a93036bcdaf9609e0f6c
InduReddyCh <S545151@nwmissouri.edu>
2021-11-29 17:46:57 -0600

    modified files

--- routes/ball.js
+++ routes/ball.js
@@ -1,6 +1,16 @@
 var express = require('express');
 const ball_controller=require('../controllers/ball');
 var router = express.Router();
+// A little function to check if we have an authorized user and continue on or
+
+// redirect to login.
+const secured = (req, res, next) => {
+  if (req.user){
+   return next();
+  }
+  req.session.returnTo = req.originalUrl;
+  res.redirect("/login");
+  }
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -12,7 +22,7 @@ module.exports = router;
 router.get('/detail', ball_controller.ball_view_one_Page);
 /* GET create ball page */
 router.get('/create', ball_controller.ball_create_Page);
-/* GET create update page */
-router.get('/update', ball_controller.ball_update_Page);
+/* GET update ball page */
+router.get('/update',secured, ball_controller.ball_update_Page);
 /* GET create ball page */
 router.get('/delete', ball_controller.ball_delete_Page);
